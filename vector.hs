bdf3c4d753c266cb6be35b92e7e8add8846c6ccd
dimitroffangel <angeldimitrov05@gmail.com>
2021-01-18 14:27:05 +0200

    added more functions to vector

--- vector.hs
+++ vector.hs
@@ -10,6 +10,7 @@ data Vector = Vector {
 
 instance Num Vector where
     (Vector x1 y1 z1) + (Vector x2 y2 z2) = Vector (x1 + x2) (y1 + y2) (z1 + z2)
+    (Vector x1 y1 z1) - (Vector x2 y2 z2) = Vector (x1 - x2) (y1 - y2) (z1 - z2)
     (Vector x1 y1 z1) * (Vector x2 y2 z2) = Vector (x1 * x2) (y1 * y2) (z1 * z2)
     negate (Vector x1 y1 z1) = Vector (-x1) (-y1) (-z1)
     abs (Vector x y z) = Vector (abs x) (abs y) (abs z)
@@ -25,6 +26,7 @@ getVectorLength a = sqrt $ getSquaredVector a
 
 getSquaredVector (Vector x y z) = x^2 + y^2 + z^2 
 
+getUnitVector vector = scalarDivision vector $ getVectorLength vector
 
 data Rgb = Rgb {    red:: Word8, 
                     green :: Word8, 
