21bc7cb268f5fb6d9f3e56a9728e41dc67b0170e
dimitroffangel <angeldimitrov05@gmail.com>
2021-01-29 04:33:47 +0200

    added bounding function and hitting function for YZRect

---
+++ RayTracer/src/YZRect.hs
@@ -0,0 +1,31 @@
+module YZRect where
+
+import AxisAlignedBoundingBox
+import HitRecord
+import Ray
+import Vector
+import HittableTypes
+
+
+xzRectBoundingBox (XZRect y0 y1 z0 z1 x _) fromInterval toInterval result =
+    Right $ AABB (Vector (x - thinBorderDistance) y0 z0) $ Vector (x - thinBorderDistance) y1 z1 
+
+hitXZRect (YZRect y0 y1 z0 z1 yzRectX yzRectMaterial) 
+ ray@(Ray rayOrigin rayDirection _) tMin tMax hitRecord@(HitRecord hitRecordPoint hitRecordNormal hitRecordMaterial u v hitRecordT hitRecordFrontFace) =
+    let t = (yzRectX - (x rayOrigin)) / (x rayDirection)
+        in if t < tMin || t > tMax 
+            then Left hitRecord
+            else 
+                let 
+                    intersectedY = (y rayOrigin) + t * (y rayDirection)
+                    intersectedZ = (z rayOrigin) + t * (z rayDirection)
+                    in if intersectedY  < y0 || intersectedX  > y1 || intersectedZ < z0 || intersectedZ > z1
+                        then Left hitRecord
+                        else 
+                            let 
+                                newU = (intersectedY - y0) / (y1 - y0)
+                                newV = (intersectedY - z0) / (z1 - z0)
+                                newT = t
+                                in Right $ 
+                                    setFaceNormal (HitRecord (getPointLocation ray t) hitRecordNormal xyRectMaterial newU newV newT hitRecordFrontFace)
+                                    ray $ Vector 0 0 1
