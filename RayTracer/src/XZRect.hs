26e0ee337d0fb0093b611d5975cbe54bd9b3740c
dimitroffangel <angeldimitrov05@gmail.com>
2021-01-29 04:29:34 +0200

    added hitXZRect

--- RayTracer/src/XZRect.hs
+++ RayTracer/src/XZRect.hs
@@ -10,3 +10,22 @@ import HittableTypes
 xzRectBoundingBox (XZRect x0 x1 z0 z1 y _) fromInterval toInterval result =
     Right $ AABB (Vector x0 (y - thinBorderDistance) z0) $ Vector x1 (y + thinBorderDistance) z1 
 
+hitXZRect (XZRect x0 x1 z0 z1 xyRectY xzRectMaterial) 
+ ray@(Ray rayOrigin rayDirection _) tMin tMax hitRecord@(HitRecord hitRecordPoint hitRecordNormal hitRecordMaterial u v hitRecordT hitRecordFrontFace) =
+    let t = (xyRectY - (y rayOrigin)) / (y rayDirection)
+        in if t < tMin || t > tMax 
+            then Left hitRecord
+            else 
+                let 
+                    intersectedX = (x rayOrigin) + t * (x rayDirection)
+                    intersectedZ = (z rayOrigin) + t * (z rayDirection)
+                    in if intersectedX  < x0 || intersectedX  > x1 || intersectedZ < z0 || intersectedZ > z1
+                        then Left hitRecord
+                        else 
+                            let 
+                                newU = (intersectedX - x0) / (x1 - x0)
+                                newV = (intersectedY - z0) / (z1 - z0)
+                                newT = t
+                                in Right $ 
+                                    setFaceNormal (HitRecord (getPointLocation ray t) hitRecordNormal xyRectMaterial newU newV newT hitRecordFrontFace)
+                                    ray $ Vector 0 0 1
